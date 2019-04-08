774de7778ede0448fb51f3d10b2db25b75013841
Pigman <ldpigma@OU133-20.local>
2019-04-08 18:30:35 -0500

    added mapkit

--- JSONparser.playground/Contents.swift
+++ JSONparser.playground/Contents.swift
@@ -1,5 +1,4 @@
 import UIKit
-
 var json = """
 {
 
@@ -38,39 +37,40 @@ var json = """
 }
 """.data(using: .utf8)
 
-//struct this:Decodable{
-//
-//    let people:[People]
+struct Parser:Codable{
 
-    struct People:Decodable{
+    let people:[People]
+
+    struct People:Codable{
         let name:String
         let age:Int
         let dog:Dog
         
-        struct Dog:Decodable {
+        struct Dog:Codable {
             let name:String
             let breed:String
             let photo:Photo
             
-            struct Photo:Decodable {
+            struct Photo:Codable {
                 let size:Size
-                
-                struct Size:Decodable {
+                let title_of_photo:String
+    
+                struct Size:Codable{
                     let height:Int
                     let width:Int
                 }
             }
         }
     }
-    
-//}
-
+}
 let decoder = JSONDecoder()
+let theGang = try decoder.decode(Parser.self, from: json!)
+var people = theGang.people
+for dudes in people{
+    print("Name: \(dudes.name)\nAge: \(dudes.age)\nDog Name: \(dudes.dog.name)\nDog Breed: \(dudes.dog.breed)\nPhoto title: \(dudes.dog.photo.title_of_photo)\nPhoto size: \(dudes.dog.photo.size.height) x \(dudes.dog.photo.size.width)\n")
+}
 
 
-let theGang = try decoder.decode(People.self, from: json!)
-
-print(theGang.age)
 
 
 
